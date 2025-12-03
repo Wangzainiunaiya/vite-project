125b0d75a503abf328cdba91bb11463920c5c2e8
songwencong <1429030586@qq.com>
2025-12-03 17:08:20 +0800

    更新 MyHeader 组件，添加导航菜单和样式

--- src/components/Layout/MyHeader.vue
+++ src/components/Layout/MyHeader.vue
@@ -1,5 +1,14 @@
 <template>
- <div>头部</div> 
+ <div>
+  <h1>My Header</h1>
+  <nav>
+   <ul>
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About</a></li>
+    <li><a href="#">Contact</a></li>
+   </ul>
+  </nav>
+ </div> 
 </template>
 
 <script setup lang='ts'>
@@ -7,5 +16,22 @@
 </script>
 
 <style scoped>
- 
+  h1 {
+    color: #333;
+  }
+  nav ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  nav ul li {
+    display: inline;
+    margin-right: 15px;
+  }
+  nav ul li a {
+    text-decoration: none;
+    color: #007BFF;
+  }
+  nav ul li a:hover {
+    text-decoration: underline;
+  }
 </style>
